d6f07bc6c8c2010abdb8761002aea58f179b7bf0
marcus.santos <marcus.santos@bycoders.com.br>
2026-01-06 16:39:13 -0300

    Add tests module

---
+++ tests.py
@@ -0,0 +1,14 @@
+import unittest
+
+from src.exceptions import UsernameException
+
+
+class TestUser(unittest.TestCase):
+
+    def test_this_works(self):
+        with self.assertRaises(UsernameException):
+            raise UsernameException()
+
+
+if __name__ == '__main__':
+    unittest.main()
